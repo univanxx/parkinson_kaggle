5b0fbce6f5ea2ceb6ba6f1ade01ef9180d655ec0
univaxx <wchhiaarid@yandex.ru>
2023-04-13 16:02:41 +0000

    logging added

--- trainer.py
+++ trainer.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 
 from models.ParkinsonBERT.transformer import BERT4Park
 from models.ParkinsonBERT.data_preparing import get_data, ParkinsonDataset
-from BERT_pytorch.bert_pytorch.trainer.optim_schedule import ScheduledOptim
+# from BERT_pytorch.bert_pytorch.trainer.optim_schedule import ScheduledOptim
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning) 
@@ -23,56 +23,58 @@ from torch.utils.tensorboard import SummaryWriter
 
 from sklearn.metrics import average_precision_score
 import os
+import logging
 
-import torch.distributed as dist
-import torch.multiprocessing as mp
+# import torch.distributed as dist
+# import torch.multiprocessing as mp
 
 
 class Trainer():
     def __init__(self, train_dataset, val_dataset, args, gpu):
-
-        torch.cuda.set_device(gpu)
+        logging.basicConfig(level=logging.INFO, filename="./model/bert_log.log", filemode="a", format="%(asctime)s %(message)s")
+        # torch.cuda.set_device(gpu)
 
         ############################################################
-        rank = args.nr * args.gpus + gpu	                          
-        dist.init_process_group(                                   
-            backend='nccl',                                         
-            init_method='env://',                                   
-            world_size=args.world_size,                              
-            rank=rank                                               
-        )                                                          
+        # rank = args.nr * args.gpus + gpu	                          
+        # dist.init_process_group(                                   
+        #     backend='nccl',                                         
+        #     init_method='env://',                                   
+        #     world_size=args.world_size,                              
+        #     rank=rank                                               
+        # )                                                          
         ############################################################ 
         # if bert_params is not None:
         #     self.model = BERT4Park(**bert_params)
         # else:
-        self.model = BERT4Park()
-
-        torch.cuda.set_device(gpu)
-        self.model.cuda(gpu)
-        self.model = nn.parallel.DistributedDataParallel(self.model.double(), device_ids = [gpu])
+        self.model = BERT4Park(seq_size=args.max_len).double()
+        self.device = torch.device("cuda:{}".format(gpu))
+        self.model.to(self.device)
+        # torch.cuda.set_device(gpu)
+        # self.model.cuda(gpu)
+        # self.model = nn.parallel.DistributedDataParallel(self.model.double(), device_ids = [gpu])
 
         self.batch_size = args.batch_size
         self.len_train = len(train_dataset)
         self.len_val = len(val_dataset)
-        train_sampler = torch.utils.data.distributed.DistributedSampler(
-            train_dataset,
-    	    num_replicas=args.world_size,
-    	    rank=rank
-        )
-        val_sampler = torch.utils.data.distributed.DistributedSampler(
-            val_dataset,
-            num_replicas=args.world_size,
-            rank=rank,
-            shuffle=False
-        )
+        # train_sampler = torch.utils.data.distributed.DistributedSampler(
+        #     train_dataset,
+    	#     num_replicas=args.world_size,
+    	#     rank=rank
+        # )
+        # val_sampler = torch.utils.data.distributed.DistributedSampler(
+        #     val_dataset,
+        #     num_replicas=args.world_size,
+        #     rank=rank,
+        #     shuffle=False
+        # )
 
-        self.train_loader = DataLoader(train_dataset, pin_memory=True, batch_size=args.batch_size, num_workers=4, sampler=train_sampler)
-        self.val_loader = DataLoader(val_dataset, pin_memory=True, batch_size=args.batch_size, num_workers=4, sampler=val_sampler)
+        self.train_loader = DataLoader(train_dataset, pin_memory=True, shuffle=True, batch_size=args.batch_size, num_workers=4)#, sampler=train_sampler)
+        self.val_loader = DataLoader(val_dataset, pin_memory=True, shuffle=False, batch_size=args.batch_size, num_workers=4)#, sampler=val_sampler)
         # self.device = torch.device("cuda:" + str(args.cuda_id) if torch.cuda.is_available() else "cpu")
         # self.model = (self.model.double()).to(self.device)
         self.optimizer = optim.AdamW(self.model.parameters(), lr=args.lr)
-        self.scheduler = ScheduledOptim(self.optimizer, 5, n_warmup_steps=100)
-        self.loss = nn.BCELoss().cuda(gpu)
+        # self.scheduler = ScheduledOptim(self.optimizer, 5, n_warmup_steps=100)
+        self.loss = nn.BCELoss()#.cuda(gpu)
 
         os.makedirs("./summary", exist_ok=True)
         os.makedirs("./model", exist_ok=True)
@@ -83,31 +85,33 @@ class Trainer():
     def train(self):
         best_val = -38
         for epoch in tqdm(range(self.num_epochs)):
+            logging.info(f"Epoch number {epoch} started")
             # training
             self.model.train()
             with tqdm(total=np.ceil(self.len_train / self.batch_size).astype(int)) as pbar:
                 for batch in self.train_loader:
-                    batch = {k: v.cuda(non_blocking=True) for k, v in batch.items()}
+                    batch = {k: v.to(self.device) for k, v in batch.items()}  # v.cuda(non_blocking=True)
                     self.optimizer.zero_grad()
                     logits = self.model(batch['value'], batch['mask'])[:,1:-1,:]
                     loss = self.loss(logits, batch['target'])
 
-                    self.scheduler.zero_grad()
+                    # self.scheduler.zero_grad()
                     loss.backward()
-                    self.scheduler.step_and_update_lr()
+                    # self.scheduler.step_and_update_lr()
 
                     self.optimizer.step()
                     self.writer.add_scalar("Train Loss", loss.item(), global_step=self.global_step)
                     self.writer.add_scalar("Train Macro AP", average_precision_score(batch['target'].reshape(batch['target'].shape[0] * batch['target'].shape[1], 4).cpu().detach(), logits.reshape(logits.shape[0] * logits.shape[1], 4).cpu().detach(), average='macro'), global_step=self.global_step)
                     self.global_step += 1
                     pbar.update(1)
+            logging.info(f"Epoch finished! Global step = {self.global_step}")
             # validating
             self.model.eval()
             val_loss = []
             with tqdm(total=np.ceil(self.len_val / self.batch_size).astype(int)) as pbar:
                 with torch.no_grad():
                     for batch in self.val_loader:
-                        batch = {k: v.cuda(non_blocking=True) for k, v in batch.items()}
+                        batch = {k: v.to(self.device) for k, v in batch.items()}
                         logits = self.model(batch['value'], batch['mask'])[:,1:-1,:]
                         val_loss.append(average_precision_score(batch['target'].reshape(batch['target'].shape[0] * batch['target'].shape[1], 4).cpu().detach(), logits.reshape(logits.shape[0] * logits.shape[1], 4).cpu().detach(), average='macro'))
                         pbar.update(1)
@@ -115,6 +119,7 @@ class Trainer():
                 torch.save(self.model.state_dict(), "./model/best_checkpoint.pth")
                 best_val = np.mean(val_loss)
             self.writer.add_scalar("Val Macro AP", np.mean(val_loss), global_step=epoch)
+            logging.info(f"Validation finished!")
 
 
 def bert_train(gpu, args):
@@ -153,11 +158,14 @@ if __name__ == "__main__":
     parser.add_argument('--random_state', default=42, type=int, 
                         help='random state')
     args = parser.parse_args()
+    args.max_len = args.max_len - 2
+    bert_train(args.gpus, args)
+    
     #########################################################
-    args.world_size = args.gpus * args.nodes                #
-    os.environ['MASTER_ADDR'] = 'localhost'                 #
-    os.environ['MASTER_PORT'] = '11238'                     #
-    mp.spawn(bert_train, nprocs=args.gpus, args=(args,))    #
+    # args.world_size = args.gpus * args.nodes                #
+    # os.environ['MASTER_ADDR'] = 'localhost'                 #
+    # os.environ['MASTER_PORT'] = '11238'                     #
+    # mp.spawn(bert_train, nprocs=args.gpus, args=(args,))    #
     #########################################################
 
 
